4e6f7caedb16ba011aca95e11b7b532a5a15c625
Volodymir Konovalov <vvkon@meta.ua>
2019-10-11 06:42:52 +0300

    Update SurceE1101.cpp

--- SurceE1101.cpp
+++ SurceE1101.cpp
@@ -10,6 +10,7 @@ int main()
     cout << "Enter a file name for reading text:\n";
     cin >> name_file;
     reading_numbers(name_file, data);
+    cout << data << '\n';  
 
     return 0;
 }
@@ -19,5 +20,6 @@ void reading_numbers(string& name_file, string& data)
     ifstream ist(name_file);
     if(!ist) error("Unable to open input file ", name_file);
     ist.exceptions(ist.exceptions() | ios_base::badbit);
-
+    getline(ist, data);
+    
 }
