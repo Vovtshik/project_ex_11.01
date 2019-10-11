4685064f0cfa6c2bf5e0edbf9a36a42b0fa9a707
Volodymir Konovalov <vvkon@meta.ua>
2019-10-11 06:33:37 +0300

    Update SurceE1101.cpp

--- SurceE1101.cpp
+++ SurceE1101.cpp
@@ -9,3 +9,10 @@ int main()
 
     return 0;
 }
+
+void reading_numbers(string& name_file, string& data)
+{
+    ifstream ist(name_file);
+    if(!ist) error("Unable to open input file ", name_file);
+    ist.exceptions(ist.exceptions() | ios_base::badbit);
+}
