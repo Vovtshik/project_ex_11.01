5e5b6f1fead4d2922b254f1ef6d02e49cfa6ff61
Volodymir Konovalov <vvkon@meta.ua>
2019-10-12 09:09:34 +0300

    Update SurceE1101.cpp

--- SurceE1101.cpp
+++ SurceE1101.cpp
@@ -6,8 +6,13 @@ void out_file_text(string& str, string& name_f);
 int main()
 {
     setlocale(LC_ALL, "Rus");
-
-
+    cout << "Enter the text: \n";
+    string str;
+    cin >> str;
+    string name_f;
+    cout << "Enter the name of the output file to write the line:\n";
+	cin >> name_f;
+    out_file_text(str, name_f);
     return 0;
 }
 
@@ -17,4 +22,14 @@ void tolower(string& s)
     {
         x = tolower(x);
     }
+}
+
+void out_file_text(string& str, string& name_f)
+{
+   ostringstream os(str);
+   ofstream ost{name_f};
+   if (!ost) error("Unable to open output file ", name_f);
+   ost << str << '\n';
+   ost << os.str();
+
 }
